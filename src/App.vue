8b6b2303a0b00b4dafa76fbcb1ff5a5ff993c34b
Dmitrii Eremin <dmitrii.eremin@withsecure.com>
2024-01-24 19:50:55 +0200

    Added database + basic UX

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <template>
   <div id="app">
-    <h1 class="fw-bold ms-2">Бобинки-пушинки</h1>
+    <header>
+      <h1 class="fw-bold ms-2">Бобинки-пушинки</h1>
+    </header>
     <!-- <price-slider :maximal.sync="maximal"></price-slider> -->
-    <product-list :products="products" :maximal="maximal"></product-list>
+    <!-- <product-list :products="products" :maximal="maximal"></product-list> -->
+
+    <product-list :products="database.products" :maximal="maximal"></product-list>
+
+    <footer>
+      <div class="contacts">
+        <p class="contacts-header">Наши контакты:</p>
+        <br>
+        <table>
+          <tbody>
+            <tr>
+              <td>Телефон:</td>
+              <td><a href="tel:79031234567">+7 (903) 123-4567</a></td>
+            </tr>
+            <tr>
+              <td>Электронная почта:</td>
+              <td><a href="mailto:internetshop416@gmail.com">internetshop416@gmail.com</a></td>
+            </tr>
+            <tr>
+              <td>WhatsApp:</td>
+              <td><a href="tel:79031234567">+7 (903) 123-4567</a></td>
+            </tr>
+            <tr>
+              <td>Telegram:</td>
+              <td><a href="telegram:internetshop416">@internetshop416</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -10,11 +41,12 @@
 import PriceSlider from "./components/PriceSlider.vue";
 import ProductList from "./components/ProductList.vue";
 import json from '../data/database.json'
+
 export default {
   name: "App",
   data: function () {
     return {
-      maximal: 30,
+      maximal: 0,
       products: [],
       database: json
     };
@@ -24,11 +56,26 @@ export default {
     PriceSlider,
   },
   mounted: function () {
-    fetch("https://hplussport.com/api/products/order/price")
-      .then((res) => res.json())
-      .then((save) => {
-        this.products = save;
-      });
   },
 };
 </script>
+
+<style>
+  header {
+    margin: 50px;
+    text-align: center;
+  }
+
+  footer {
+    margin: 50px;
+  }
+
+  .contacts-header {
+    text-align: center;
+  }
+
+  .contacts {
+    width: 400px;
+    margin: auto;
+  }
+</style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,20 +2,45 @@
   <div id="productList" class="container">
     <div
       class="container d-flex align-items-center gap-2 mt-2"
-      v-for="(item, index) in showItem"
+      v-for="(item, index) in filteredItems"
       :key="index"
     >
       <img :src="item.image" class="img-fluid col-md-3 col-sm-3" />
       <div class="col-md-9 p-2 w-50 col-sm-8">
-        <h5>
-          {{ item.name }}
-        </h5>
-        <p>
-          {{ item.description }}
-        </p>
-        <b>
-          {{ item.price }}
-        </b>
+        <h5>{{ item.name }}</h5>
+        <table>
+          <tbody>
+            <tr>
+              <td>Артикул:</td>
+              <td>{{ item.id }}</td>
+            </tr>
+            <tr>
+              <td>Цвет:</td>
+              <td>{{ item.color }}</td>
+            </tr>
+            <tr>
+              <td>Состав:</td>
+              <td>{{ item.id }}</td>
+            </tr>
+            <tr>
+              <td>В наличии:</td>
+              <td>{{ item.available }} грамм</td>
+            </tr>
+            <tr>
+              <td>Длина нити:</td>
+              <td>{{ item.length }}</td>
+            </tr>
+            <tr>
+              <td>Описание:</td>
+              <td>{{ item.description }}</td>
+            </tr>
+            <tr>
+              <td>Цена:</td>
+              <td><strong>{{ item.price }} руб.</strong> за {{ item.weight_for_price }} грамм</td>
+            </tr>
+          </tbody>
+        </table>
+        <button>Заказать</button>
       </div>
     </div>
   </div>
@@ -26,10 +51,10 @@ export default {
   name: "product-list",
   props: ["products", "maximal"],
   computed: {
-    showItem: function () {
-      let max = this.maximal;
+    filteredItems: function () {
+      const max_price = this.maximal;
       return this.products.filter(function (item) {
-        return item.price <= max;
+        return max_price <= 0 || item.price <= max_price;
       });
     },
   },

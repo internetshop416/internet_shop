cc8924a8f78a04ed15df5141138edb0ef2da7590
Dmitrii Eremin <dmitrii.eremin@withsecure.com>
2024-01-25 18:13:15 +0200

    breadcrumbs

--- src/components/TypeSelector.vue
+++ src/components/TypeSelector.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <section class="type-selector">
-      <div v-for="(type, index) in types" :key="index">
-        <button @click="onTypeClicked(type.id)">{{ type.name }}</button>
-      </div>
+      <ul class="breadcrumb">
+        <li v-for="(type, index) in types" :key="index">
+          <a v-if="index !== currentIndex" href="javascript:void(0)" @click="onTypeClicked(type.id)">{{ type.name }}</a>
+          <span v-else class="current-link">{{ type.name }}</span>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -15,12 +18,13 @@ export default {
   emits: ['typeChanged'],
   methods: {
     onTypeClicked: function(id) {
+      this.currentIndex = id;
       this.$emit('typeChanged', id);
     }
   },
   data: function () {
     return {
-      maxAmount: 30,
+      currentIndex: 0
     };
   },
 };
@@ -30,6 +34,48 @@ export default {
 .type-selector {
   background-color: white;
   color: black;
+  width: 1290px;
+  margin: 0 auto;
+  border-radius: 5px;
+}
+
+/* Style the list */
+ul.breadcrumb {
+  padding: 10px 16px;
+  list-style: none;
+  background-color: white;
+  display:flex;
+  justify-content: center;
+  border-radius: 5px;
+}
+
+/* Display list items side by side */
+ul.breadcrumb li {
+  display: inline;
+  font-size: 18px;
+}
+
+/* Add a slash symbol (/) before/behind each list item */
+ul.breadcrumb li+li:before {
+  padding: 8px;
+  color: black;
+  content: "|\00a0";
+}
+
+/* Add a color to all links inside the list */
+ul.breadcrumb li a {
+  color: #0275d8;
+  text-decoration: none;
+}
+
+/* Add a color on mouse-over */
+ul.breadcrumb li a:hover {
+  color: #01447e;
+  text-decoration: underline;
+}
+
+.current-link {
+  color: #aaa !important;
 }
 </style>
   
